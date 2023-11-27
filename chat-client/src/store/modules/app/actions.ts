c5fad51062461e7a9a3495df7b2705618b82ad4c
junqi1121 <1320208060@qq.com>
2023-11-27 23:34:34 +0800

    前端框架初步，需要改成stomp协议，以支持 springboot后端，TODO！

---
+++ chat-client/src/store/modules/app/actions.ts
@@ -0,0 +1,38 @@
+import { SET_USER, SET_TOKEN } from './mutation-types';
+import { ActionTree } from 'vuex';
+import { AppState } from './state';
+import { RootState } from '../../index';
+import fetch from '@/api/fetch';
+import { processReturn } from '@/utils/common.ts';
+
+const actions: ActionTree<AppState, RootState> = {
+  async register({ commit }, payload) {
+    let res = await fetch.post('/auth/register', {
+      ...payload,
+    });
+    let data = processReturn(res);
+    if (data) {
+      commit(SET_USER, data.user);
+      commit(SET_TOKEN, data.token);
+      return data;
+    }
+  },
+  async login({ commit }, payload) {
+    let res = await fetch.post('/auth/login', {
+      ...payload,
+    });
+    let data = processReturn(res);
+
+    if (data) {
+      commit(SET_USER, data.user);
+      commit(SET_TOKEN, data.token);
+      return data;
+    }
+    // 否则打印错误信息到控制台
+    else {
+      console.log(data);
+    }
+  },
+};
+
+export default actions;
